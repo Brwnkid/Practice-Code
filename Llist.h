cf40f67efa36f7a4ec872ed12190e24c15aad1a0
Brwnkid <shahmk88@gmail.com>
2015-07-06 14:01:51 -0400

    Made into a templated class and added prepend fuction

--- Llist.h
+++ Llist.h
@@ -1,13 +1,41 @@
-#include <iostream>
+template<class G> class Node{
+public:
+  Node();
+  ~Node(delete next);
+  G value;
+  Node*<G> next;
+}
 
 template <class T>
 class Llist{
 public:
-	Llist();
-	void push(T const&);
+	Llist(){
+  head = NULL;
+  tail = NULL;
+  mySize = 0;
+  }
+  ~Llist(){
+    delete head;
+  }
+	void prepend(T &value){
+    if(head == NULL){ //list is empty
+      head = new Node*<T>;
+      head.next = NULL;
+      head.value = value;
+      tail = head;
+      mySize++;
+      return;
+    }
+    Node*<T> newNode = new Node*<T>;
+    newNode.value = value;
+    newNode.next = head;
+    head = newNode;
+  }
 	void pop();
 	bool isEmpty();
 	T top() const;
 private:
-	
+	Node*<T> head;
+  Node*<T> tail;
+  unsigned int mySize = 0;
 };
